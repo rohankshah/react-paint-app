587495bab2809984b0b81370146812baa5c01334
Rohan Shah <moreinmore@gmail.com>
2023-06-07 18:56:24 +0530

    added select and edit button toggles

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,40 +2,20 @@ import React, { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { fabric } from "fabric";
 import { setCanvasObj } from "../actions/canvas-actions";
-import {
-  handleMouseDown,
-  handleMouseMove,
-  handleMouseUp,
-} from "../actions/line-actions";
 
-function Canvas() {
+function Canvas(props) {
   const canvasRef = useRef(null);
   const dispatch = useDispatch();
 
-  const vw = Math.max(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  const vh = Math.max(
-    document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
-  );
-  const canvasWidth = vw * 0.95;
-  const canvasHeight = vh * 0.8;
-
   useEffect(() => {
     let canvas = new fabric.Canvas(canvasRef.current);
-    canvas.setDimensions({ height: canvasHeight, width: canvasWidth });
+    canvas.setBackgroundColor("#ffffff");
+    canvas.setDimensions({
+      height: props.canvasHeight,
+      width: props.canvasWidth,
+    });
     canvasRef.current.style.border = "2px solid black";
     dispatch(setCanvasObj(canvas));
-
-    // canvas.on("mouse:down", (options) =>
-    //   dispatch(handleMouseDown(canvas, options))
-    // );
-
-    // canvas.on("mouse:move", (options) => dispatch(handleMouseMove(options)));
-
-    // canvas.on("mouse:up", (options) => dispatch(handleMouseUp()));
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
--- src/pages/PaintPage.js
+++ src/pages/PaintPage.js
@@ -6,22 +6,39 @@ import {
   handleMouseMove,
   handleMouseUp,
 } from "../actions/line-actions";
+import { pencilIcon, selectionIcon } from "../svg/allSvg";
 
 const styles = {
   mainCont: {
-    height: "100%",
-    width: "100%",
+    height: "100vh",
+    width: "100vw",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "center",
+    backgroundColor: "#D3D3D3",
   },
 
   toolBoxCont: {
-    height: "18%",
-    width: "95%",
-    padding: "1em",
-    border: "1px solid black",
+    height: "3em",
+    width: "95vw",
+    borderLeft: "2px solid black",
+    borderRight: "2px solid black",
+    borderTop: "2px solid black",
+    display: "flex",
+    alignItems: "center",
+    padding: "0em 1em",
+    backgroundColor: "white",
+  },
+
+  buttonUnclick: {
+    backgroundColor: "#a3a3a3",
+    marginRight: "1em",
+  },
+
+  buttonClick: {
+    backgroundColor: "#5a5a5a",
+    marginRight: "1em",
   },
 };
 
@@ -30,11 +47,60 @@ function PaintPage() {
   const dispatch = useDispatch();
 
   const [drawToggle, setDrawToggle] = useState(false);
+  const [buttonToggle, setButtonToggle] = useState([
+    { id: "select", clicked: false },
+    { id: "edit", clicked: false },
+  ]);
+
+  function handleButtonClick(e) {
+    switch (e.currentTarget.title) {
+      case "select":
+        console.log("select button pressed");
+        break;
+      case "edit":
+        console.log("edit button pressed");
+        break;
+      default:
+        break;
+    }
+    const updatedButtons = buttonToggle.map((button) => {
+      if (button.id === e.currentTarget.title) {
+        return { ...button, clicked: true };
+      }
+      return { ...button, clicked: false };
+    });
+
+    setButtonToggle(updatedButtons);
+  }
+
+  function handleEditButton() {
+    currentCanvasObj._setOptions({ selection: false });
+    currentCanvasObj.on("mouse:down", (options) =>
+      dispatch(handleMouseDown(currentCanvasObj, options))
+    );
+    currentCanvasObj.on("mouse:move", (options) =>
+      dispatch(handleMouseMove(options))
+    );
+    currentCanvasObj.on("mouse:up", (options) =>
+      dispatch(handleMouseUp(currentCanvasObj, options))
+    );
+  }
+
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  const vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+  const canvasWidth = vw * 0.95;
+  const canvasHeight = vh * 0.8;
 
   useEffect(() => {
     if (Object.keys(currentCanvasObj).length !== 0) {
       if (drawToggle) {
-        console.log(currentCanvasObj);
+        currentCanvasObj._setOptions({ selection: false });
         currentCanvasObj.on("mouse:down", (options) =>
           dispatch(handleMouseDown(currentCanvasObj, options))
         );
@@ -44,7 +110,8 @@ function PaintPage() {
         currentCanvasObj.on("mouse:up", (options) =>
           dispatch(handleMouseUp(currentCanvasObj, options))
         );
-      } else {
+      } else if (!drawToggle) {
+        currentCanvasObj._setOptions({ selection: true });
         currentCanvasObj.__eventListeners = {};
       }
     }
@@ -52,10 +119,37 @@ function PaintPage() {
 
   return (
     <div style={styles.mainCont}>
-      <div style={styles.toolBoxCont}>
-        <button onClick={() => setDrawToggle(!drawToggle)}>Draw Line</button>
+      <div>
+        <div style={styles.toolBoxCont}>
+          <div
+            // onClick={() => setDrawToggle(false)}
+            // style={drawToggle ? styles.buttonUnclick : styles.buttonClick}
+            onClick={(e) => handleButtonClick(e)}
+            style={
+              buttonToggle.filter((ele) => ele.id === "select")[0].clicked
+                ? styles.buttonClick
+                : styles.buttonUnclick
+            }
+            title="select"
+          >
+            {selectionIcon()}
+          </div>
+          <div
+            // onClick={() => setDrawToggle(!drawToggle)}
+            // style={drawToggle ? styles.buttonClick : styles.buttonUnclick}
+            onClick={(e) => handleButtonClick(e)}
+            style={
+              buttonToggle.filter((ele) => ele.id === "edit")[0].clicked
+                ? styles.buttonClick
+                : styles.buttonUnclick
+            }
+            title="edit"
+          >
+            {pencilIcon()}
+          </div>
+        </div>
+        <Canvas canvasHeight={canvasHeight} canvasWidth={canvasWidth} />
       </div>
-      <Canvas />
     </div>
   );
 }
